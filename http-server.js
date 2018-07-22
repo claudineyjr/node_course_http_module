43abddc753d32c8364f1e4f25bd11fd2b845dda2
Claudiney Junior <claudiney.alves@fatec.sp.gov.br>
2018-07-22 12:55:34 -0300

    Mostrando Headers, Método e URL através da requisição

---
+++ http-server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((request, respose) => {
+  console.log('Requisição feita');
+  const { headers, method, url } = request;
+  console.log('Headers:')
+  console.log(headers);
+  console.log('Método:');
+  console.log(method);
+  console.log('URL:');
+  console.log(url);
+
+});
+
+server.listen(3000);
