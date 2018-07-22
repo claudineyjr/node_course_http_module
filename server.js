95c004cfd43807d6895a5604b4197907327ac0ea
Claudiney Junior <claudiney.alves@fatec.sp.gov.br>
2018-07-22 13:18:59 -0300

    Servidor considerando URL e Método da requisição

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  const { url } = req;
-  if (url === '/course') {
+  const { url, method } = req;
+  if (url === '/course' && method === 'GET') {
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1><b>Ola Pessoal!</b></h1>');
   }
 
-  if (url === '/students') {
+  if (url === '/course' && method === 'POST') {
+    res.setHeader('Content-type', 'text/html');
+    res.end('<h1><b>Não é possível criar um novo curso agora!</b></h1>');
+  }
+
+  if (url === '/students' && method === 'GET') {
     res.setHeader('Content-Type', 'application/json');
     const students = [
       { id: 1, name: 'Maria'},
